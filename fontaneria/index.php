5515a99baa72c170bc055bd35b90bc277383c15e
JaimeASU <102379592+JaimeASU@users.noreply.github.com>
2022-03-28 11:55:13 -0600

    Add files via upload

---
+++ fontaneria/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+    <header>
+      <div class="header text-center">
+        <img src="img/header.jpg" class="w-100" alt="">
+      </div>
+      <div class="nav">
+        <nav>
+          <a href="#">Inicio</a>
+          <a href="#">Servicios</a>
+          <a href="#">Contácto</a>
+        </nav>
+      </div>
+    </header>
+
+    <main class="">
+      <div class="contenedor infop">
+        <h1 class=" ">Hacemos Diagnóstico con Equipo de Ultrasonido, en el cuál revisamos: Muebles Sanitarios, Aljibe, Tinaco y Tuberías. Encontrando la causa del Problema.</h1>
+      </div>
+
+      <div class="contenedor fugas-loc">
+        <h2 class="text-center">TUBERÍAS ROTAS BAJO EL PISO</h2>
+        <div class="fugas">
+          <div class="info">
+            <h3>Realizamos inspección de tuberías ocultas con equipo de ultrasonido.</h3>
+            <h3 class="">Localizamos la Fuga, hacemos el arreglo de la tubería y resanamos el piso. Solo se hace excavación exactamente donde esté la Fuga.</h3>
+          </div>
+          <div class="imagen">
+            <img src="img/fuga-loc.jpg" class="w-100" alt="">
+          </div>
+        </div>
+      </div>
+    </main>
+
+
+
+    <script type="text/javascript" src="js/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
